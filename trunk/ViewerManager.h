9bb5c3a542b2edf9b03913caf3405a99a325b3db
halprin <halprin@67cec062-a9fc-4cbc-9cd1-c969c0405a8e>
2008-03-05 00:44:28 +0000

    Made program using bindings and an Array Controller.  Still needing implementation is the filtering, credits, and saving IIRC.

--- trunk/ViewerManager.h
+++ trunk/ViewerManager.h
@@ -3,6 +3,7 @@
 #import <Cocoa/Cocoa.h>
 #import "Loader.h"
 #import "Preferences.h"
+#import "ManEntry.h"
 
 @interface ViewerManager : NSObject
 {
@@ -13,17 +14,20 @@
 	IBOutlet NSWindow *window;
 	IBOutlet Loader *loader;
 	IBOutlet Preferences *preferences;
-	NSMutableArray *manlist;
+	IBOutlet NSArrayController* manlist;
 	NSMutableArray *searchDirectories;
+	BOOL loaded;
+	NSString* searchString;
+	NSString* filterString;
 }
 -(ViewerManager*)init;
--(int)numberOfRowsInTableView: (NSTableView*)aTableView;
--(id)tableView: (NSTableView*)aTableView objectValueForTableColumn: (NSTableColumn*)aTableColumn row: (int)rowIndex;
--(void)tableView: (NSTableView*)aTableView setObjectValue: (id)anObject forTableColumn: (NSTableColumn*)aTableColumn row: (int)rowIndex;
--(void)addEntry: (NSString*)name withReload: (BOOL)flag;
+-(void)tableViewSelectionDidChange: (NSNotification*)notification;
+-(void)addEntry: (NSString*)name withSection: (NSString*)section;
 -(void)applicationDidFinishLaunching: (NSNotification*)notification;
 -(void)applicationWillTerminate: (NSNotification*)notification;
 -(IBAction)showPreferences: (id)sender;
+-(IBAction)search: (id)sender;
+-(IBAction)filter: (id)sender;
 -(IBAction)update: (id)sender;
 -(void)load;
 -(void)dealloc;
